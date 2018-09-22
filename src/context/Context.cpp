b17fc48f9c467947220d1470ef2c49fe8e042598
Loulfy <loulfy.dev@gmail.com>
2018-09-22 17:40:52 +0200

    prepare dir

---
+++ src/context/Context.cpp
@@ -0,0 +1,10 @@
+//
+// Created by loulfy on 22/09/18.
+//
+
+#include "Context.hpp"
+
+namespace lyr
+{
+
+}
---
+++ src/context/Context.hpp
@@ -0,0 +1,16 @@
+//
+// Created by loulfy on 22/09/18.
+//
+
+#ifndef LYR_CONTEXT_HPP
+#define LYR_CONTEXT_HPP
+
+namespace lyr
+{
+    class Context
+    {
+
+    };
+}
+
+#endif //LYR_CONTEXT_HPP
